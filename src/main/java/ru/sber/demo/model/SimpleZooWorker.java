4db16a5ec7c5e06d287da0e7b003f81d96915986
Eugene Zubenko <zubenko.evgeny@gmail.com>
2024-01-16 00:05:59 +0300

    DSL'less cases fix.

--- src/main/java/ru/sber/demo/model/SimpleZooWorker.java
+++ src/main/java/ru/sber/demo/model/SimpleZooWorker.java
@@ -4,7 +4,15 @@ import ru.sber.demo.model.api.ZooWorker;
 
 import javax.annotation.PostConstruct;
 
+import java.util.Set;
+
 public class SimpleZooWorker implements ZooWorker {
+    
+    private final Set<String> pokemonNames;
+    
+    public SimpleZooWorker(Set<String> pokemonNames) {
+        this.pokemonNames = pokemonNames;
+    }
     
     @PostConstruct
     public void init() {
@@ -13,6 +21,10 @@ public class SimpleZooWorker implements ZooWorker {
     
     @Override
     public void work() {
+        System.out.println("==================");
         System.out.println("Опять работать((");
+        System.out.println("Покемоны, мои покемоны...");
+        System.out.println("Вот они, слева направо: " + String.join(", ", pokemonNames));
+        System.out.println("==================");
     }
 }
